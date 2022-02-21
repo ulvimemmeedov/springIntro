2df112d07bf3f6d35ed78d545233df8b20dd863f
Ulvi Memmedov <onezerolord@gmail.com>
2022-02-21 17:35:09 +0400

    post product

--- src/main/java/com/springintro/intro/entities/concretes/Product.java
+++ src/main/java/com/springintro/intro/entities/concretes/Product.java
@@ -5,18 +5,23 @@ package com.springintro.intro.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "product_id")
     private int id;
     
